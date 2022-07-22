536d6b6961deb7c01817d9934d9668ed78e5c907
Zachary Baumgarten <zach.baumgarten@yahoo.com>
2022-07-22 18:20:15 -0500

    basic model added for now

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .catch(err => console.error(err));
 
 // test
-application.get('/', (req, res) => {
-    res.send('Hello, there!')
-})
+// application.get('/', (req, res) => {
+//     res.send('Hello, there!')
+// })
 
 // port is set to 3000 in my .env for now
 const PORT = process.env.PORT
