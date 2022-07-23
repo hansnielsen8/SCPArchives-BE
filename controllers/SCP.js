a49ef0554f92ffcca2919b7ec7391ce9ff34e4ef
Zachary Baumgarten <zach.baumgarten@yahoo.com>
2022-07-23 08:14:30 -0500

    finished controller skeleton, will need more work

--- controllers/SCP.js
+++ controllers/SCP.js
@@ -1,10 +1,66 @@
 // dependancies
 const router =  require('express').Router()
-const scp = require('../models/SCP-Model')
+const SCP = require('../models/SCP-Model')
 
-// this should hopefully be the home page
+// this should hopefully be the home page, tested and works
 router.get('/', (req, res) => {
     res.send('Home!')
+})
+
+// scp list page named "SCP Archive", tested and works
+router.get('/scp', async (req, res) => {
+    try {
+        const SCPs = await SCP.find().populate('SCP')
+
+        res.json(SCPs)
+    } catch (error) {
+        res.status(500).json({ "message": "error accessing SCP archives" })
+    }
+})
+
+// indiviual scp page, page you are taken to when you select one of SCPs from the archive page. not yet tested
+router.get('/scp/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const scp = await SCP.findOne({ id })
+
+        res.json(scp)
+    } catch (error) {
+        res.status(500).json({ "message": "error accessing SCP entry" })
+    }
+})
+
+// delete SCP entry, not yet tested
+router.delete('/scp/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const scp = await SCP.findOneAndDelete({ id })
+
+        res.json(scp)
+    } catch (error) {
+        res.status(500).json({ "message": "error deleting SCP entry" })
+    }
+})
+
+// SCP create page labeled roughly "New Archive Entry", not yet tested
+router.post('/new_entry', async (req, res) => {
+    try {
+        const { id, name, location, date, image, description, containment } = req.body
+
+        const createNewSCPEntry = await new SCP({
+            id,
+            name,
+            location,
+            date,
+            image,
+            description,
+            containment
+        }).save()
+
+        res.json({ 'message': 'New SCP entry added'})
+    } catch (error) {
+        res.status(400).json({ "message": "error adding SCP entry" })
+    }
 })
 
 module.exports = router
